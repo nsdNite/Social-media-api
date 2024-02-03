08e9116c1723f28e930ae2c4922d4d7b04f5dea4
nsdNite <nsd7night@gmail.com>
2024-02-03 21:09:25 +0200

    Implement registration with email
    Run migrations for user

--- social_media_service/settings.py
+++ social_media_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "social_media",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
