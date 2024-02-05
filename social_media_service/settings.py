c1fc717e94062e3c7366cba8cc5a66d2baacdd3a
nsdNite <nsd7night@gmail.com>
2024-02-05 09:30:38 +0200

    Update settings with media and interal host

--- social_media_service/settings.py
+++ social_media_service/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -103,6 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "user.User"
+
+MEDIA_URL = "/uploads/"
+MEDIA_ROOT = BASE_DIR / "uploads"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
