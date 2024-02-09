00dfb9e067f6510ea0981c17326c812da0ae6194
nsdNite <nsd7night@gmail.com>
2024-02-09 22:23:34 +0200

    Add like and comment registration to admin.py

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from social_media.models import Profile, Post, Comment, ScheduledPost
+from social_media.models import Profile, Post, Comment, ScheduledPost, Like, Follow
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ScheduledPost)
+admin.site.register(Like)
+admin.site.register(Follow)
