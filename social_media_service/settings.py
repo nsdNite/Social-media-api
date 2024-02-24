29ab096aa23a8c4b681e6dee393e16710752e5f8
nsdNite <nsd7night@gmail.com>
2024-02-24 19:23:10 +0200

    Reformat few files, add info to .env-sample

--- social_media_service/settings.py
+++ social_media_service/settings.py
@@ -19,7 +19,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
